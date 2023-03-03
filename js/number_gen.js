9713b7aa7e0425758b5ff6668225ec598b4ea628
Aswinkumaresan <aswinabi2000@gmail.com>
2023-03-03 14:38:37 +0530

    number game updated

--- js/number_gen.js
+++ js/number_gen.js
@@ -5,11 +5,12 @@ function randomNumShow() {
 }
 
 // Display data in input
-
+let dis_number;
 function display() {
   let random = randomNumShow();
-  let dis_number = document.getElementById("number_input");
-  dis_number.value = random;
+  dis_number = document.getElementById("number_input");
+  // dis_number.value = random;
+  dis_number.innerText = random;
 
   let randomarray = [];
 
@@ -30,7 +31,10 @@ function display() {
       let headEm = document.createElement("h2");
 
       colEm.setAttribute("class", "displaydata col-4");
+      colEm.setAttribute("data-target", elm);
+      colEm.onclick = findNumber;
 
+      // headEm.innerText = elm;
       headEm.innerText = randomarray[i];
 
       colEm.appendChild(headEm);
@@ -41,5 +45,40 @@ function display() {
 }
 
 dis_trigger.addEventListener("click", display);
+
+
+count=0;
+function findNumber(event){
+  let num=event.target.dataset.target;
+  count +=1;
+  if(count<=3){
+    if(num == dis_number.innerText){
+      event.target.classList.add("active_success");
+    }
+    else{ 
+      event.target.classList.add("active");
+    }
+    alert("Your  chance count is "+ count + "/3")
+  }
+  else{
+    alert("Your chance is closed by 3/3" +"Try by refreshing");
+  }
+  
+ 
+  console.log(num);
+}
+
+// function findNumber(eventshow){
+//   // let num=eventshow.target.dataset.target;
+//   // console.log(eventshow.target.dataset.target);
+
+//   // if(num == dis_number.value){
+//     // eventshow.target.classList("active_success")
+//   }
+  // else{
+    // eventshow.target.classList("active")
+//   }
+  
+// }
 
 // show data in div
